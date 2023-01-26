a4a0de1b80326e2c46bff8e8dd1b8126b1d3ca78
alejandromsagui <alex.maeso19@gmail.com>
2023-01-26 13:17:00 +0100

    Añadida las dos animaciones y el diseño inicial del login

--- src/views/Vw-Index.vue
+++ src/views/Vw-Index.vue
@@ -5,12 +5,28 @@
     </video>
 
     <div class="center-button">
-        <button class="button" @click="credentials()">Login</button>
+
         <transition-group appear @before-enter="beforeEnter" @enter="enter">
-            <v-card text="..." v-if="clicked">
-                <v-card-actions>
-                    <v-btn>Click me</v-btn>
-                </v-card-actions>
+            <button type="button" class="button" @click="corangeentials()" key="button">ACCEDER</button>
+        </transition-group>
+
+    </div>
+
+    <div class="center-card">
+        <transition-group appear @before-enter="beforeLogin" @enter="enterLogin">
+            <v-card class="card" v-if="clicked" :elevation="24">
+                <v-card-title id="card-title">
+                    <span>Iniciar sesión</span>
+                </v-card-title>
+                <div class="background-card">
+                    <v-form ref="form" lazy-validation id="form">
+                    <v-responsive class="mx-auto" max-width="350">
+                        <v-text-field width="20px" label="Usuario" requiorange></v-text-field>
+                        <v-text-field type="password" label="Contraseña" requiorange></v-text-field>
+                    </v-responsive>
+                </v-form>
+                <v-btn variant="outlined" id="buttonLoggin">Entrar</v-btn>
+                </div>
             </v-card>
         </transition-group>
     </div>
@@ -22,26 +38,50 @@ import { gsap } from 'gsap';
 
 const clicked = ref(false);
 
-const credentials = () => {
+const corangeentials = () => {
     clicked.value = true;
-    console.log("prueba");
+
+    gsap.to(".button", {
+        opacity: 0,
+        x: 700,
+        duration: 1,
+        ease: "power2.inOut"
+    })
+
 }
 
 const beforeEnter = (el) => {
     el.style.opacity = 0;
-    el.style.transform = 'translateY(-60px)'
+    el.style.transform = 'translateY(-100px)'
 }
-//Aquí comienza la animación
+
 const enter = (el, done) => {
     gsap.to(el, {
         opacity: 1,
         y: 0,
-        duration: 0.8,
-        delay: el.dataset.index * 0.4,
-        onComplete: done
+        duration: 1,
+        delay: 0.4,
+        onComplete: done,
+        ease: 'bounce.out'
     })
 }
 
+const beforeLogin = (el) => {
+    el.style.opacity = 0;
+    el.style.transform = 'translateY(-60px)'
+}
+
+const enterLogin = (el) => {
+    if (clicked.value == true) {
+        gsap.to(el, {
+            opacity: 1,
+            y: 0,
+            duration: 1,
+            delay: 0.4,
+            ease: 'bounce.out'
+        })
+    }
+}
 
 </script>
 
@@ -71,32 +111,66 @@ const enter = (el, done) => {
     color: white;
     font-weight: bolder;
     font-size: larger;
-    border: solid 4px red;
-    height: 60px;
-    width: 190px;
-    background-color: red;
+    border: solid 4px orange;
+    height: 100px;
+    width: 500px;
+    background-color: orange;
     position: absolute;
-    left: 50%;
     top: 50%;
     transform: translate(-50%, -50%);
     border-radius: 4px;
     padding: 20px 30px;
     letter-spacing: 1.5px;
-    background: linear-gradient(to left, white 50%, red 50%) right;
+    background: linear-gradient(to left, white 50%, orange 50%) right;
     background-size: 200% 100%;
-    font-size: 1.5em;
+    font-size: 2.5em;
     margin: 0 auto;
     padding: 0;
     display: inline-block;
     line-height: 50px;
-    text-align: center;
     color: black;
-
 }
 
 .button:hover {
     transition: 0.5s;
     background-position: left;
     color: white;
+}
+
+.center-card {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.card {
+    position: absolute;
+    top: 35%;
+    transform: translate(-50%, -50%);
+    width: 22%;
+    height: 33%;
+
+}
+
+#card-title {
+    background-color: orange;
+    color: white;
+    font-weight: bold;
+    padding: 15px;
+    padding-bottom: 17px;
+}
+
+#card-title>span {
+    margin-left: 15px;
+}
+#buttonLoggin {
+    background-color: orange;
+    display: flex;
+    margin: 0 auto;
+    color: white;
+    font-weight: bold;
+}
+#form {
+    margin-top: 40px;
 }
 </style>
